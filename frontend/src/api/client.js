cc4f810cd1adb93800587b962990073bad50b101
Your Name <youremail@example.com>
2026-09-06 09:48:56 +0530

    Update frontend API URL

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: "https://cloud-storage-service-p6fx.onrender.com",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
